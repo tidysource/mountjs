0eb87248011659131005bcc1473ea88094e627b7
danielcobo <danielcobo@users.noreply.github.com>
2015-11-28 10:57:02 +0100

    Outline set property methods

--- m.js
+++ m.js
@@ -7,7 +7,7 @@ m.module = function(module){
 	Validate module
 	---------------
 	*/
-	//<--- add validation
+	//<--- add validation & conform module params
 	
 	/*
 	Build reference to specified path
@@ -72,7 +72,81 @@ m.module = function(module){
 	Property methods are functions that set parameter
 	values for our action function, e.g.:
 	m.param1().param2.doSomething();
+	
+	Property methods are set on module.path under 
+	.m.protoObj and under module.path.paramName by
+	reference
 	*/
+	for (var i=0;i<module.param.length;++i){
+		var param = module.param[i];
+		
+		//<---check if default value conforms (if not try to conform it)
+		
+		//Check for property method clash
+		if (typeof ref[param.name] === 'undefined'){
+			//No clash - set property method
+			(function(param){
+			protoObj[param.name] = function(val){
+				if (typeof val === 'undefined'){
+					//Return value
+					
+					//If undefined return default value
+					if (typeof this.m.args[param.name] === 'undefined'
+					 && typeof param.auto !== 'undefined'){
+						return param.auto;
+					}
+					else{
+						return this.m.args[param.name];
+					}
+				}
+				else{
+					//Set value
+					
+					//Validate value to be set
+					if (typeof param.type !== 'undefined'){
+						for (var j=0;j<param.type.length;++j){
+							
+						}
+					}
+
+					//Conform value 
+					if (typeof param.conform === 'string'){
+						//<--- check if val can be conformed to conform value
+					}
+					
+					//Set value;
+					var inst = this;
+					
+					/*
+					Check if a new instance should be created
+					
+					Note:
+					A new instance is needed the first time 
+					a property method is called.
+					*/
+					if (typeof this.m.args === 'undefined'){
+						inst = Object.create(this);
+						inst.m.args = {};
+					}
+					
+					inst.m.args[param.name] = val;
+					
+					return inst;					
+				}
+			};
+			
+			//Set reference on to protoObj
+			ref[param.name] = protoObj[param.name];
+			})(param);
+		}
+		else 
+		if (JSON.stringify(param)
+		!== JSON.stringify(protoObj[param.name].m)){
+			//Property method is already defined and differs
+			throw new Error('Incompatible param.');
+		}
+		//else do nothing, same property method already set
+	}
 	
 	/*
 	Set action method
